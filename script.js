e647c5eb71ea3f207c0b011bcf033fc9970dc070
Taka Sasaki <kashira2339@gmail.com>
2018-12-14 14:20:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const space = str => str + " ";
 
 const world = str => str + "world";
 
+const exclamationAsync = async str =>
+  new Promise((resolve, reject) => setTimeout(() => resolve(str + "!"), 300));
+
+const toUppercase = str => str.toUpperCase();
+
 const str = "";
 
 const msg1 = world(space(hello(str)));
 const msg2 = str |> hello |> space |> world;
+const msg3 = str |> hello |> space |> world |> exclamationAsync |> await |> toUppercase;
 
-console.log("old:", msg1);
-console.log("new:", msg2);
+console.log("msg1:", msg1); // => msg1: hello world
+console.log("msg2:", msg2); // => msg2: hello world
+console.log("msg3:", msg3); // => msg3: HELLO WORLD!
